b84ac7999183e866d49960855c8c1d2e96a64ab9
JuferBlue <2846823009@qq.com>
2024-07-31 20:35:52 +0800

    初始化测试

---
+++ BasicAlgorithm/src/排序/快速排序.java
@@ -0,0 +1,13 @@
+package 排序;
+
+/**
+ * @BelongsProject: JavaAlgorithm
+ * @BelongsPackage: 排序
+ * @Author: JuferBlue
+ * @CreateTime: 2024-07-31  20:34
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class 快速排序 {
+
+}
